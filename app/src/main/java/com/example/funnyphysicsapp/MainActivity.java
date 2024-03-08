9c1f2429337a7000726af5caee90f017363b4043
Mahmud Jumyazov <mahmudjumyazov919@gmail.com>
2024-03-08 16:00:17 +0500

    First Commit

---
+++ app/src/main/java/com/example/funnyphysicsapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.funnyphysicsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
+import android.os.Bundle;
+import android.text.TextPaint;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
